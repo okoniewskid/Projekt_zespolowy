2d468568d2f1b36bebb7eb046f3380dd06259f66
okoniewskid <okoniewskid@gmail.com>
2016-03-21 20:40:38 +0100

    changed graphics, added some tweaks, started working on deck

---
+++ Unity/TCG/Assets/Scripts/Card.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+
+    public Sprite[] faces;
+    public Sprite cardBack;
+
+    public int cardIndex;
+
+    public void ToggleFace(bool showFace)
+    {
+
+
+        if(showFace)
+        {
+            spriteRenderer.sprite = faces[cardIndex];
+        }
+        else
+        {
+            spriteRenderer.sprite = cardBack;
+        }
+    }
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+}
---
+++ Unity/TCG/Assets/Scripts/DebugChangeCard.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugChangeCard : MonoBehaviour
+{
+    Card cardModel;
+    int cardIndex = 0;
+
+    public GameObject card;
+    
+    void Awake()
+    {
+        cardModel = card.GetComponent<Card>();
+    }
+
+    void OnGUI()
+    {
+        if(GUI.Button(new Rect(10, 10, 100, 28), "Draw a card"))
+        {
+            cardModel.cardIndex = cardIndex;
+            cardModel.ToggleFace(true);
+
+            cardIndex++;
+
+            if(cardIndex == 52)
+            {
+                cardIndex = 0;
+                cardModel.ToggleFace(false);
+            }
+        }
+    }
+}
